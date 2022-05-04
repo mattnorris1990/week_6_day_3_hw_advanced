96c66c957ec8104cd9bba1f2780385440daec60e
mattnorris1990 <mattnorris1990@gmail.com>
2022-05-04 16:57:12 +0100

    pangram passing all tests

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,17 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase
 }
 
 PangramFinder.prototype.isPangram = function () {
+  lowerCasePhrase = this.phrase.toLowerCase()
 
+  result = this.alphabet.every((letter) => {
+    return lowerCasePhrase.includes(letter)
+  })
+
+  return result
+  
 }
 
 module.exports = PangramFinder;
