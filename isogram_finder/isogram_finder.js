d2b4892edcf348b371d67824d89df44a0b4a3889
mattnorris1990 <mattnorris1990@gmail.com>
2022-05-04 21:27:22 +0100

    started Pangram

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,16 @@
 const IsogramFinder = function (word) {
-
+    this.word = word;
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+    resultSet = [... new Set(this.word.split(""))]
+    console.log(resultSet)
+    console.log(this.word.split(""))
+    if (this.word.split("") == resultSet) {
+        return true
+    } else {
+        return false
+    }
 }
 
 module.exports = IsogramFinder;
