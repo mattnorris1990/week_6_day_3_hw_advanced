2ae3845c5b434acad987d9e84dcb24da67ead806
mattnorris1990 <mattnorris1990@gmail.com>
2022-05-05 09:52:50 +0100

    initial isogram solve added

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -4,13 +4,15 @@ const IsogramFinder = function (word) {
 
 IsogramFinder.prototype.isIsogram = function () {
     resultSet = [... new Set(this.word.split(""))]
+    comparison = this.word.split("")
     console.log(resultSet)
     console.log(this.word.split(""))
-    if (this.word.split("") == resultSet) {
+    if (comparison.length === resultSet.length) {
         return true
     } else {
         return false
     }
+
 }
 
 module.exports = IsogramFinder;
